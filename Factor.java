a498ce95d318dfdef694d086d80828b968928b4f
aswindevmp <72143783+aswindevmp@users.noreply.github.com>
2020-10-29 10:32:49 +0530

    Add files via upload

---
+++ Factor.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Factor{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    System.out.println("Enter a value for N: ");
+    int x;
+    x = sc.nextInt();
+    sc.close();
+    System.out.printf("Factors of %d  are :-",x);
+    for(int i=1; i<=x; i++)
+    {
+        if(x%i==0)
+            System.out.print(i+" ");
+    }
+
+    
+}
+}
